1974d683a239075139dbd75fdf27d7c45c4be57a
Greg Gould <greg.gould90@gmail.com>
2016-05-17 21:12:33 -0230

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,8 @@ set :deploy_to, '/home/deploy/test_app'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :rbenv_ruby, '2.3.0'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
